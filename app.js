af987cab20b857e2ce23d3e4a03a699f9040f874
Ismail Muhammed <ismailmuhammed2019@gmail.com>
2021-06-01 10:54:32 +0100

    Added elements to the app and created a reset button for input

--- app.js
+++ app.js
@@ -1,11 +1,41 @@
 document.addEventListener('DOMContentLoaded', () => {
+//create const
   const list = document.querySelector('.inner-wrapper ul');
   const forms = document.forms;
   
+  //delete task
   list.addEventListener('click', (e)=>{
-      if(e.target.className == 'delete'){
+      if(e.target.className == 'deleted'){
           const li = e.target.parentElement;
           li.parentNode.removeChild(li);
       }
   });
+
+
+  //add movie
+  const addForm = forms['myform'];
+  addForm.addEventListener('submit', (e)=>{
+  e.preventDefault();
+
+  //creating elements
+const value = addForm.querySelector('input[type="text"]').value;
+const li = document.createElement('li');
+const title = document.createElement('span');
+const deleted = document.createElement('span');
+  //add text content
+title.textContent = value;
+deleted.textContent = 'Delete'
+  //add classes
+    title.classList.add('title');
+    deleted.classList.add('deleted');
+  //append to Dom
+  li.appendChild(title);
+  li.appendChild(deleted);
+  list.appendChild(li);
+
+  addForm.reset();
+});
+
+
+
 })
